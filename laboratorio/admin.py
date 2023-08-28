e26e9cb10d9d34021613591da1960255f8cbb449
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2023-08-28 19:57:19 -0400

    init

---
+++ laboratorio/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+
+from .models import Laboratorio, DirectorGeneral, Producto
+
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+
+admin.site.register(Laboratorio, LaboratorioAdmin)
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio')
+    ordering = ('nombre', )
+
+admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio', 
+        'f_fabricacion_year', 'p_costo', 'p_venta')
+    
+    ordering = ('nombre', 'laboratorio')
+
+    list_display_links = ('nombre', 'laboratorio')
+
+    list_filter = ('nombre', 'laboratorio')
+
+    def f_fabricacion_year(self, obj):
+        return obj.f_fabricacion.year
+    
+    f_fabricacion_year.short_description = "F Fabricacion"
+
+admin.site.register(Producto, ProductoAdmin)
---
+++ laboratorio/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Laboratorio(models.Model):
+    nombre = models.CharField(max_length = 128)
+    ciudad = models.CharField(max_length = 128, default = "")
+    pais = models.CharField(max_length = 128, default = "") 
+
+    def __str__(self):
+        return self.nombre
+
+class DirectorGeneral(models.Model):
+    nombre = models.CharField(max_length = 128)
+    especialidad = models.CharField(max_length = 128, default = "") 
+    laboratorio = models.OneToOneField(Laboratorio, on_delete = models.PROTECT)
+
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length = 128)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete = models.PROTECT)
+    f_fabricacion = models.DateField()
+    p_costo = models.DecimalField(max_digits = 12, decimal_places = 2)
+    p_venta = models.DecimalField(max_digits = 12, decimal_places = 2)
+
+    def __str__(self):
+        return self.nombre
